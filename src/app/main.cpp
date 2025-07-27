4b1bd7d42c3aa2fa247425782c4327f0e0733b07
Blue Whale <gbabatz_cs@proton.me>
2025-07-27 23:48:19 +0300

    Use basic logging

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
-// #include <filesystem>
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
 
 int main() {
-  std::cout << "this is main.cpp running" << std::endl;
+  // Initialize the logger  
+  std::shared_ptr<spdlog::logger> logger;
+  try {
+    logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
+  } catch (const spdlog::spdlog_ex &ex) {
+    std::cout << "Log init failed: " << ex.what() << std::endl;
+  }
+  if (!logger) {
+    std::cerr << "Logger initialization failed." << std::endl;
+    return 1;
+  }
+  spdlog::set_default_logger(logger); // this redirects all logs to the default logger
+  // logger->info("Application started"); // alternative way to log messages with log object
+  // logger->debug("Debugging information: main.cpp is running");
+
+  spdlog::set_level(spdlog::level::debug); // this makes the debug log messages visible
+  spdlog::info("Application started");
+  spdlog::debug("Debugging information: main.cpp is running");
+
   return 0;
 }
