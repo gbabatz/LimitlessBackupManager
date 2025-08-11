09d21d8e98752c2356f56d16b9a99fcd06f6739d
Blue Whale <gbabatz_cs@proton.me>
2025-08-11 19:59:00 +0300

    Add cxxopts module to build

---
+++ dependencies/install-cxxopts-locally.sh
@@ -0,0 +1,5 @@
+# this is a header only library
+# it is included in the project already - this script is for educatinoal purposes only
+# cd project root folder
+mkdir -p external/cxxopts/include
+curl -o external/cxxopts/include/cxxopts.hpp https://raw.githubusercontent.com/jarro2783/cxxopts/refs/tags/v3.3.1/include/cxxopts.hpp
